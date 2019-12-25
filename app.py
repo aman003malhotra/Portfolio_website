3324f2c12b3e38be68f4eafbccfc3df64b67d2a2
Aman Malhotra <aman003malhotra@gmail.com>
2019-12-26 00:14:37 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template,redirect, url_for, request, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+from database import get_db, connect_db
+import os
+# import ScrollReveal from 'scrollre'
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+	if request.method == 'POST':
+		db = get_db()
+		db.execute('insert into project_enteries(name, email, message) values (?, ?, ?)',[request.form['name'],request.form['email'],request.form['message']])
+		db.commit()
+		flash("Thanks For Feeding the information We will reach to you soon", 'success')
+		return redirect(url_for('index'))
+
+	return render_template('index.html', title='Aman Malhotra')
+
+if __name__ == '__main__':
+	app.run(debug=True)
---
+++ schema.sql
@@ -0,0 +1,5 @@
+create table project_enteries(
+	id integer primary key autoincrement,
+	name text not null,
+	email text not null,
+	message text not null);
